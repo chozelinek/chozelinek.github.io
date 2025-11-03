544b52045262fdd9df01f77ff57ed270fc4015c5
José Manuel Martínez Martínez <chozelinek@gmail.com>
2025-11-03 18:30:00 +0100

    2025 update

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Distributing Python modules for development",
+        "excerpt":"Sometimes you write or (more likely to be my case) modify already existing python modules to suit your needs. If these modifications are crucial in a particular pipeline that you want others to reproduce (like in a research paper) or you are using that module over and over again, it...","categories": ["dev"],
+        "tags": ["python"],
+        "url": "/dev/2017/07/03/distributing-python-modules.html",
+        "teaser": null
+      }]
