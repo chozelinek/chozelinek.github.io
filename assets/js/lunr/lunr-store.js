e2f417768e6c43691104978d890ae6e87c6cb105
José Manuel Martínez Martínez <chozelinek@gmail.com>
2025-11-04 10:29:23 +0100

    My first post in 2025

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
         "title": "Distributing Python modules for development",
-        "excerpt":"Sometimes you write or (more likely to be my case) modify already existing python modules to suit your needs. If these modifications are crucial in a particular pipeline that you want others to reproduce (like in a research paper) or you are using that module over and over again, it...","categories": ["dev"],
-        "tags": ["python"],
-        "url": "/dev/2017/07/03/distributing-python-modules.html",
+        "excerpt":"Sometimes you write or (more likely to be my case) modify already existing python modules to suit your needs. If these modifications are crucial in a particular pipeline that you want others to reproduce (like in a research paper) or you are using that module over and over again, it...","categories": ["Development"],
+        "tags": ["Python"],
+        "url": "/development/2017/07/03/distributing-python-modules.html",
+        "teaser": null
+      },{
+        "title": "My favourite communities in Valencia",
+        "excerpt":"Recently, I embarked on a mission to explore Valencia’s vibrant tech community, diving into meetups and discovering some incredible new groups! I’m excited to share my top picks that capture the spirit of innovation across AI, Data Science, Machine Learning, and Product here in Valencia. The flag emojis indicate the...","categories": ["Professional"],
+        "tags": ["Valencia","AI","DataScience","MachineLearning","TechCommunity","Innovation","Meetups","Python","Networking"],
+        "url": "/professional/2025/11/04/my-favourite-communities-in-valencia.html",
         "teaser": null
       }]
